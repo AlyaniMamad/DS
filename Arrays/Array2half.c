0fec138449f9d6caaf26bf81908eed0209c4d2d5
Alyani Mamad <97268754+AlyaniMamad@users.noreply.github.com>
2023-08-10 23:50:01 +0530

    Arrays qnas added

---
+++ Arrays/Array2half.c
@@ -0,0 +1,40 @@
+// Input : arr[6] = [1, 90, 34, 89, 7, 9]
+// Output : 1 7 9 90 89 34
+
+#include <stdio.h>
+
+int main(){
+
+    int arr[6] = {1, 90, 34, 89, 7, 9};
+
+
+    int n = sizeof(arr)/sizeof(int);
+    int mid = n/2;
+
+    for(int i = 0 ; i<n ; i++){
+            printf("%d ",arr[i]);
+        }
+        printf("\n");
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<mid;j++){
+            if(arr[i]>arr[j]){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        for(int j=mid;j<n-1;j++){
+            if(arr[i]<arr[j]){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    for(int i = 0 ; i<n ; i++){
+            printf("%d ",arr[i]);
+        }
+
+    return 0;
+}
